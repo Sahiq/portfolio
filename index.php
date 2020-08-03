118b283bba8e5d7ddbe2e6c152f748cef8fab19f
Sahiq <saifi.sahiq@gmail.com>
2020-08-03 21:38:00 +0530

    make GoogleMapResponsive

--- index.php
+++ index.php
@@ -671,7 +671,7 @@
 					<div class="col-md-12">
 						<iframe
 							src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d224611.80435841178!2d77.17864292988016!3d28.40237151681207!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cdc15f5a424b1%3A0xe4f50576c850e0f2!2sFaridabad%2C%20Haryana!5e0!3m2!1sen!2sin!4v1596447179596!5m2!1sen!2sin"
-							width="1000" height="450" frameborder="0" style="border:0;" allowfullscreen=""
+							width="100%" height="400px" frameborder="0" style="border:0;" allowfullscreen=""
 							aria-hidden="false" tabindex="0"></iframe>
 					</div>
 				</div>
